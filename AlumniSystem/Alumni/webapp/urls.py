f6823bdfae3c85a64d36fdacce575164d48f4ca4
openred <openredshaw@163.com>
2018-01-15 18:39:17 +0800

    Add QQ login to login/register
    2018.01.11-2018.01.15

---
+++ AlumniSystem/Alumni/webapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    # url(r'^qqlogin/$', qq_login, name='qq_login'),
+    # url(r'^oauth/qq/login', qq_login, name='qq_login'),
+    # url(r'^oauth/qq/check', qq_check, name='qq_check'),
+    # url(r'^oauth/bind/account', bind_account, name='bind_account'),
+]
---
+++ AlumniSystem/README.md
@@ -0,0 +1,7 @@
+## AlumniSystem
+
+2018.01.10 Create a new Django project
+
+2018.01.11 Add login/register
+
+2018.01.15 Add QQ login to login/register
